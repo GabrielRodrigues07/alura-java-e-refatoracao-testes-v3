1828055d5ff3c11fb0eb2a19efb12be853ef36fe
Gabriel <grodrigues256@gmail.com>
2023-11-25 18:00:39 -0300

    test(m04-#25-11-23) Usando @spy do mockito para criar testes

--- src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
+++ src/test/java/br/com/alura/adopet/AdopetApiApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.alura.adopet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AdopetApiApplicationTests {
 
 	@Test
--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -14,10 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class AdocaoServiceTest {
@@ -46,8 +49,12 @@ class AdocaoServiceTest {
 
     private SolicitacaoAdocaoDto solicitacaoAdocaoDto;
 
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
     @Mock
-    private List<ValidacaoSolicitacaoAdocao> validacoes;
+    private ValidacaoSolicitacaoAdocao validador1;
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
 
     @Captor
     private ArgumentCaptor<Adocao> adocaoCaptor;
@@ -72,6 +79,26 @@ class AdocaoServiceTest {
         assertEquals(pet, adocaoSalva.getPet());
         assertEquals(tutor, adocaoSalva.getTutor());
         assertEquals(solicitacaoAdocaoDto.motivo(), adocaoSalva.getMotivo());
+
+    }
+
+    @Test
+    void deveriaChamarValidadoresDeAdocaoAoSolicitar() {
+
+        //ARRANGE
+        this.solicitacaoAdocaoDto = new SolicitacaoAdocaoDto(10L, 20L, "motivo qualquer");
+        given(petRepository.getReferenceById(solicitacaoAdocaoDto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(solicitacaoAdocaoDto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
+        validacoes.addAll(Arrays.asList(validador1, validador2));
+
+        //ACT
+        service.solicitar(solicitacaoAdocaoDto);
+
+        //ASSERT
+        then(validador1).should().validar(solicitacaoAdocaoDto);
+        then(validador2).should().validar(solicitacaoAdocaoDto);
 
     }
 
