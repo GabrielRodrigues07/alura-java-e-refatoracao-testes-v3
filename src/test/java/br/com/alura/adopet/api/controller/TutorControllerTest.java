30f7b1df46af49fe839fcab38e7858f60cb1d706
Gabriel <grodrigues256@gmail.com>
2023-12-03 12:30:21 -0300

    test(m06-#03-12-23) Terminando testes das classes de service(abrigo, tutor, pet).

--- src/test/java/br/com/alura/adopet/api/controller/TutorControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/TutorControllerTest.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
